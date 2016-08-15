4271f36bbf3b6b91bb800929cfaefd1b4ebedbef
Victor Benavente <vbenavente@hotmail.com>
2016-08-15 14:32:24 -0700

    updated gitignore, added test file, and linked list module

---
+++ src/linked_list.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Node(object):
+    def __init__(self, value, next_node):
+        self.value = value
+        self.next_node = next_node
+
+
+class LinkedList(object):
+    def __init__(self):
+
+
+    def insert(value, next=None):
+
+    def remove(value):
+
+    def search(value):
+
+    def display():
+
+    def pop():
