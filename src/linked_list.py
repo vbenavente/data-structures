218d3dfe762581962fc66d776ec76f7c67b28fbd
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-16 14:38:05 -0700

    edited docstrings, added test for pop method

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
+"""This file implements a basic linked list data structure."""
 import sys
 
+
 class Node(object):
-    """This sets our Node class"""
+    """This builds our Node class."""
+
     def __init__(self, data):
+        """Initial setup of Node objects."""
         self.data = data
         self.next_node = None
 
 
 class LinkedList(object):
+    """This builds LinkedList class."""
+
     def __init__(self, data):
+        """Initial setup of LinkedList class."""
         self.head = None
         try:
             if data:
@@ -18,25 +25,26 @@ class LinkedList(object):
         except TypeError:
             print('Please enter an iterable object.')
 
-
     def push(self, data):
+        """Function builds push method of LinkedList class."""
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
         return self.head
 
     def pop(self):
+        """Function builds pop method of LinkedList class."""
         pop_node = self.head
         self.head = self.head.next_node
         return pop_node
 
-    # def size()
-    #
+    # def size():
+
     # def search(data)
     #
     # def remove(node)
     #
-    # def display()
+    # def display():
 
 # if __name__ == '__main__':
 #     LinkedList(sys.argv[1])
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -14,15 +14,16 @@ TEST_PUSH_DATA = [
     ("cat", "dog", "dog"),
     ("z", [1, 2, 3], [1, 2, 3])
 ]
+TEST_POP_DATA = [
+    ("zebra", "a"),
+    ("encyclopedia", "a"),
+    ([1, 3, 5], 5)
+]
 
 
 @pytest.mark.parametrize("data, output", TEST_LINKED_LIST_INIT_DATA)
 def test_linked_list_init(data, output):
-    """Summary.
-
-    Returns:
-        TYPE: Description
-    """
+    """Test the __init__ of LinkedList class."""
     test_case = LinkedList(data)
     assert test_case.head.data == output
 
@@ -33,3 +34,14 @@ def test_push(data, data2, output):
     test_case = LinkedList(data)
     test_case.push(data2)
     assert test_case.head.data == output
+
+
+@pytest.mark.parametrize("data, output", TEST_POP_DATA)
+def test_pop(data, output):
+    """Test the pop method of LinkedList class."""
+    test_case = LinkedList(data)
+    assert test_case.pop().data == output
+
+# @pytest.mark.parametrize("initial, search, output", TEST_REMOVE_DATA)
+# def test_remove(initial, search, output):
+    # """Test the remove method of LinkedList class."""
