22b3075b6daa318cbe36dd0a2003d32736457a86
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-27 19:30:25 -0700

    basic graph structure laid out, some initial methods complete

--- src/simple_graph.py
+++ src/simple_graph.py
@@ -6,26 +6,61 @@ from __future__ import unicode_literals
 class SimpleGraph(object):
     """Simple_graph implements a simple graph data structure.
 
-    We support 1 way edges and lots of methods described below."""
+    We support 1 way edges and lots of methods described below.
+
+    Sample structure illustrates the concept.  Nodes a, b, c and d exist
+    and could have any valid python value.  The sub-list contains
+    edges for every node.  For instance, node_a shares an edge with
+    node_b, but node_b has no edge with node_a.  Node_b has no edges
+    of it's own, but node_a, c and d are all neighbors of b.
+
+    Graph:  {
+                "node_a": [node_b],
+                "node_b": [],
+                "node_c": [node_b],
+                "node_d": [node_a, node_b, node_c],
+            }
+    """
 
     def __init__(self):
-        """Initialize a simple_graph class instance."""
-        pass
+        """Initialize an empty simple_graph class instance.
+
+        We don't accept a value here, you must start with an empty graph
+        and add data manually via public methods."""
+        self.graph = {}
 
     def nodes(self):
         """Returns a list of all nodes in the graph."""
+        node_list = []
+        for i in self.graph:
+            node_list.append(self.graph[i])
+        return node_list
 
     def edges(self):
         """Returns a list of all edges in the graph."""
+        edge_list = []
+        for i in self.graph:
+            for j in self.graph[i]:
+                edge_list.append(self.graph[i], self.graph[i][j])
+        return edge_list
 
     def add_node(self, node):
         """Adds a new node to the graph."""
+        if node in self.graph:
+            raise IndexError("That node already exists dumbo.")
+        else:
+            self.graph[node] = []
 
     def add_edge(self, node_1, node_2):
         """Adds an edge between node_1 and node_2.
 
         If node_1 or node_2 are not present, function adds them to the graph
         first.  Edges are single direction only."""
+        if node_1 not in self.graph:
+            self.add_node(node_1)
+        if node_2 not in self.graph:
+            self.add_node(node_2)
+        self.graph[node_1].append(node_2)
 
     def del_node(self, node):
         """Deletes a node from the graph.
@@ -36,6 +71,7 @@ class SimpleGraph(object):
     def del_edge(self, node_1, node_2):
         """Deletes and edge between node_1 and node_2.
 
+        Only removes the edge in the direction node_1 -->  node_2.
         Raises a IndexError if that edge does not exist in graph."""
 
     def has_node(self, node):
--- src/test_simple_graph.py
+++ src/test_simple_graph.py
@@ -1,3 +1,72 @@
 # -*- coding: utf-8 -*-
 """File tests our simple graph data structure."""
 from __future__ import unicode_literals
+import pytest
+
+
+def test_sg_init():
+    """Ensure init returns a valid list at self.graph."""
+
+
+def test_sg_nodes():
+    """Ensure nodes returns a correct list of nodes in graph."""
+
+
+def test_sg_edges():
+    """Ensure edges returns a correct list of edges in graph."""
+
+
+def test_sg_add_node():
+    """Ensure new node is added to the graph."""
+
+
+def test_sg_add_node_error():
+    """Ensure duplicate node raises IndexError."""
+
+
+def test_sg_add_edge():
+    """Ensure new edge is added to the graph."""
+
+
+def test_sg_del_node():
+    """Ensure correct node is deleted, and all its edges are removed."""
+
+
+def test_sg_del_node_error():
+    """Ensure del_node throws IndexError if called with node not in graph."""
+
+
+def test_sg_del_edge():
+    """Ensure supplied edge is deleted from appropriate node."""
+
+
+def test_sg_del_edge_error():
+    """Ensure del_edge throws IndexError if called with node not in graph."""
+
+
+def test_sg_has_node_true():
+    """Ensure has_node returns true when called with node in graph."""
+
+
+def test_sg_has_node_false():
+    """Ensure has_node returns false when called with node not in graph."""
+
+
+def test_sg_neighbors():
+    """Ensure neighbors for node that exists are returned."""
+
+
+def test_sg_neighbors_error():
+    """Ensure error is raised if node does not exist in graph."""
+
+
+def test_sg_adjacent_true():
+    """Ensure adjacent returns true if a and b share an edge."""
+
+
+def test_sg_adjacent_false():
+    """Ensure adjacent returns false if a and b don't share an edge."""
+
+
+def test_sg_adjace_error():
+    """Ensure adjacent raises an error if either node doesn't exist."""
