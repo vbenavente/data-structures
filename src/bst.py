828e1087e9ea10a677ab91b5c72a2400c9e238b7
Derek Hewitt <derekmhewitt@gmail.com>
2016-09-13 14:10:22 -0700

    made changes based on code review today

--- src/bst.py
+++ src/bst.py
@@ -5,8 +5,8 @@ from __future__ import unicode_literals
 
 class Node(object):
 
-    def __init__(self, val, left=None, right=None,
-                 parent=None, data=None):
+    def __init__(self, val, data=None, left=None, right=None,
+                 parent=None):
         """Instantiates a Node class object for use in our BST.
 
         Data is a ride-along additional value holder that may be used
@@ -17,6 +17,7 @@ class Node(object):
         self.right = right
         self.parent = parent
         self.data = data
+        self.depth = 1
 
         def has_left_child(self):
             return self.left.val
@@ -35,34 +36,10 @@ class BinarySearchTree(object):
         self.length = 0
         if root:
             self.root = Node(root)
-            self.length += 1
+            self.length = 1
         else:
             self.root = root
-
-    def __iter__(self):
-        """Allows this data structures to be iterated on."""
-        return self.root.__iter__()
-
-    def node_list(self, starting_point=None):
-        """Steps through the BST depth-first and returns a list of nodes
-        in the BST.
-
-        Expects a starting point == value of a node in the graph."""
-        from dll import DoublyLinkedList
-        dll = DoublyLinkedList()
-        if starting_point is None:
-            starting_point = self.root
-        # if self.has_node(starting_point) is False:
-        #     raise IndexError("That starting point is not in the graph.")
-        dll.push(starting_point)
-        result = []
-        while dll.size() > 0:
-            working_node = dll.pop()
-            if working_node not in result:
-                result.append(working_node)
-                for node in self.neighbors(working_node):
-                    dll.push(node)
-        return result
+            self.length = 0
 
     def insert(self, val, data=None):
         """Inserts a value  into the BST.  If the value is already in
@@ -74,16 +51,25 @@ class BinarySearchTree(object):
         self.length += 1
 
     def _insert(self, val, current_node, data=None):
-        if val < current_node.val:
-            if current_node.has_left_child():
-                self._insert(val, data, current_node.left)
+        keep_going = True
+        while keep_going:
+            if val < current_node.val:
+                if current_node.has_left_child():
+                    new_depth = self._insert(val, data, current_node.left)
+                else:
+                    current_node.left = Node(val, data, parent=current_node)
+                    keep_going = False
+            elif val > current_node.val:
+                if current_node.has_right_child():
+                    new_depth = self._insert(val, data, current_node)
+                else:
+                    current_node.right = Node(val, data, parent=current_node)
+                    keep_going = False
             else:
-                current_node.left = Node(val, data, parent=current_node)
-        else:
-            if current_node.has_right_child():
-                self._insert(val, data, current_node)
-            else:
-                current_node.right = Node(val, data, parent=current_node)
+                keep_going = False
+        self.length += 1
+        self.depth = new_depth + 1
+        return self.depth
 
     def contains(self, val):
         """Will return True if val is in the BST, or False if it's not."""
@@ -100,52 +86,18 @@ class BinarySearchTree(object):
         """Calls self.size and returns value from there."""
         return self.size()
 
-    def depth(self):
+    def depth(self, starting_point=None):
         """Will return the depth of the tree by counting "levels".  An empty
         BST will return 0, a BST with 1 value will return 1, a BST with 2
         values will return 2, and the value will fluctuate from then on
         depending on the branch layout."""
+        if starting_point is None:
+            starting_point = self.root
+        return max(starting_point.left, starting_point.right)
 
     def balance(self, starting_point=None):
         """Will return an integer that's positive or negative that represents
         the difference between depth on both sides from the root."""
         if starting_point is None:
             starting_point = self.root
-        left_total = self._balance(starting_point.left)
-        right_total = self._balance(starting_point.right)
-        return left_total - right_total
-
-    def _balance(self, starting_point):
-        """Steps through the BST breadth-first and returns the len of the
-        resulting list.
-
-        Expects a starting point == value of a node in the BST.  This mehod
-        adapted from Derek's previous simple_graph."""
-        from dll import DoublyLinkedList
-        dll = DoublyLinkedList()
-        if self.contains(starting_point) is False:
-            raise IndexError("That starting point is not in the graph.")
-        dll.push(starting_point)
-        result = []
-        while dll.size() > 0:
-            working_node = dll.shift()
-            if working_node not in result:
-                result.append(working_node)
-                for node in self._neighbors(working_node):
-                    dll.push(node)
-        return len(result)
-
-    def _neighbors(self, node):
-        """Finds and returns the children of a given node."""
-        neighbors = []
-        if node.left:
-            neighbors.append(node.left)
-        if node.right:
-            neighbors.append(node.right)
-        return neighbors
-
-    def count(self, starting_point=None):
-        """Recursively explores the BST and returns a count of the nodes
-        'below' the starting point in the BST."""
-        if starting_point is None:
-            starting_point = self.root
+        return starting_point.left - starting_point.right
