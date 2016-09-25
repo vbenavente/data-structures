25c498465fc4285c917a9a8d5101b2352cc7534c
Victor Benavente <vbenavente@hotmail.com>
2016-09-25 03:45:13 -0700

    refactored insert_sort, printing best and worst case scenarios

--- src/insert_sort.py
+++ src/insert_sort.py
@@ -1,11 +1,32 @@
+# -*- coding: utf-8 -*-
+import timeit
+
+
+best_case = [x for x in range(0, 1000)]
+worst_case = best_case[::-1]
+
+
 def insert_sort(l):
-    """docstring"""
-    nl = []
-    for x in l:
-        for y in nl:
-            if x < y:
-                nl.insert(nl.index(y), x)
-                break
-        else:
-            nl.append(x)
-    return nl
+    """Sort a list by inserting values in order after comparing each value."""
+    if len(l) <= 1:
+        return
+    else:
+        for idx in range(1, len(l)):
+            cur = l[idx]
+            spot = idx
+            while spot > 0 and l[spot - 1] > cur:
+                l[spot] = l[spot - 1]
+                spot = spot - 1
+            l[spot] = cur
+
+
+if __name__ == '__main__':
+    print("Best Case: a list already in order")
+    print("number of runs: 1000 / average time: " + str((timeit.Timer(
+        "insert_sort(best_case)", setup="from __main__ import insert_sort, best_case").timeit(
+            number=1000))/1000))
+    print("Worst Case: a list in reverse order")
+    print("number of runs: 1000 / average time: " + str(
+        (timeit.Timer(
+            "insert_sort(worst_case)", setup="from __main__ import insert_sort, worst_case").timeit(
+                number=1000))/1000))
--- src/test_insert_sort.py
+++ src/test_insert_sort.py
@@ -3,12 +3,28 @@ from __future__ import unicode_literals
 import random
 import pytest
 
+
 TEST_LIST = [
-    (random.sample(range(100), random.randint(1, 50)))
+    [], [8]
 ]
+
+for x in range(0, 10):
+    y = (random.sample(range(100), random.randint(1, 50)))
+    TEST_LIST.append(y)
 
 
 @pytest.mark.parametrize('test_list', TEST_LIST)
 def test_insertion_sort_random(test_list):
     from insert_sort import insert_sort
-    assert insert_sort(test_list) == sorted(test_list)
+    insert_sort(test_list)
+    assert test_list == sorted(test_list)
+
+
+def test_best_case():
+    from insert_sort import best_case
+    assert type(best_case) is list
+
+
+def test_worst_case():
+    from insert_sort import worst_case
+    assert type(worst_case) is list
