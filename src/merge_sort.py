a5ae0c09c1aeb621b2de671e1628ddab2d074350
Victor Benavente <vbenavente@hotmail.com>
2016-09-25 19:58:09 -0700

    started work on merge_sort

---
+++ src/merge_sort.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+def merge_sort(a_list):
+    new_list = []
+    for i in a_list:
+        sub_list = [i]
+        new_list.append(sub_list)
+        
---
+++ src/test_merge_sort.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+def test_merge_sort():
+    """Test merge sort sorts any list passed in."""
+    
