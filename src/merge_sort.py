1b9c9275fa5e2db178053bbc2757cd69420835b7
Victor Benavente <vbenavente@hotmail.com>
2016-09-26 03:52:09 -0700

    merge sort and helper function working, all tests passing

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -1,9 +1,37 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 
 def merge_sort(a_list):
+    """Use merge sort to sort the list."""
+    if len(a_list) <= 1:
+        return a_list
+    left = []
+    right = []
+    for i in range(len(a_list)):
+        if i % 2 != 0:
+            left.append(a_list[i])
+        else:
+            right.append(a_list[i])
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return _merge(left, right)
+
+
+def _merge(left, right):
+    """Takes two lists and returns single sorted list."""
     new_list = []
-    for i in a_list:
-        sub_list = [i]
-        new_list.append(sub_list)
-        
+    while left and right:
+        if left[0] < right[0]:
+            new_list.append(left[0])
+            left = left[1:]
+        else:
+            new_list.append(right[0])
+            right = right[1:]
+    while left:
+        new_list.append(left[0])
+        left = left[1:]
+    while right:
+        new_list.append(right[0])
+        right = right[1:]
+    return new_list
--- src/test_merge_sort.py
+++ src/test_merge_sort.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from merge_sort import merge_sort
 
 
 def test_merge_sort():
     """Test merge sort sorts any list passed in."""
-    
+    result = merge_sort([3, 8, 2, 82, 38])
+    assert result == sorted([3, 8, 2, 82, 38])
