132c09eeb1d328c72a3176801fde41cfb0eb00ed
Victor Benavente <vbenavente@hotmail.com>
2016-10-04 14:48:29 -0700

    added initial methods to write on hash table class

---
+++ src/hash_table.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class HashTable(object):
+    """Implement a hash table class in Python."""
+    def __init__(self, iterable=None):
+        """Set up initialization method on hash table class."""
+        self.hash_list = []
+
+    def _hash(self, key):
+        """Hash the keys in our hash table."""
+        pass
+
+    def get(self, key):
+        """Returns the value associated with passed key."""
+        pass
+
+    def set(self, key, val):
+        """Adds a new key, value pair to hash table."""
+        pass
--- src/radix_sort.py
+++ src/radix_sort.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 from collections import deque, OrderedDict
 import timeit
