0c66badc0ad9e91a55b5c13ffd43505ac03ca23f
Steven Than <steventhan11@gmail.com>
2016-10-21 21:16:41 -0700

    second hashing function

--- src/hash_table.py
+++ src/hash_table.py
@@ -4,9 +4,18 @@
 
 class HashTable(object):
     """Implement a hash table class in Python."""
-    def __init__(self, iterable=None):
+    def __init__(self, iterable=None, size=10, hashing_algorithm='default'):
         """Set up initialization method on hash table class."""
-        self.hash_list = [[] for _ in range(10)]
+        algorithms = {
+            'default': '_hash',
+            'strong': '_hash_not_naive'
+        }
+        try:
+            self.hashing_algorithm = algorithms[hashing_algorithm]
+        except KeyError as e:
+            e.args = ('Hashing algorithm not found', )
+            raise
+        self.hash_list = [[] for _ in range(size)]
 
     def _hash(self, key):
         """Hash the keys in our hash table."""
@@ -20,12 +29,18 @@ class HashTable(object):
             return hashed_value
 
     def _hash_not_naive(self, key):
-        """Hash the keys in our hash table in a better way."""
-        pass
+        """Hash the keys in our hash table in a different way."""
+        if isinstance(key, int):
+            return key
+        elif isinstance(key, str):
+            hashed_value = ''
+            for c in key:
+                hashed_value += str(ord(c))
+            return int(hashed_value)
 
     def get(self, key):
         """Returns the value associated with passed key."""
-        hashed_key = self._hash(key)
+        hashed_key = getattr(self, self.hashing_algorithm)(key)
         bucket_idx = hashed_key % len(self.hash_list)
         for k, v in self.hash_list[bucket_idx]:
             if k == key:
@@ -33,6 +48,6 @@ class HashTable(object):
 
     def set(self, key, val):
         """Adds a new key, value pair to hash table."""
-        hashed_key = self._hash(key)
+        hashed_key = getattr(self, self.hashing_algorithm)(key)
         bucket_idx = hashed_key % len(self.hash_list)
         self.hash_list[bucket_idx].append((key, val))
--- src/test_hash_table.py
+++ src/test_hash_table.py
@@ -10,15 +10,46 @@ def create_ht():
     return HashTable()
 
 
+@pytest.fixture(scope="function")
+def second_ht():
+    """Create instance of a hash table with the 2nd hash function."""
+    return HashTable(hashing_algorithm='strong')
+
+
 def test_hash_table_init(create_ht):
     """Test our init sets up a hash_list attribute that is a list."""
     assert isinstance(create_ht.hash_list, list)
+    assert '_hash' in create_ht.hashing_algorithm
+
+
+def test_hash_table_init_2nd_hash(second_ht):
+    """Test our init sets up a hash_list attribute that is a list."""
+    assert isinstance(second_ht.hash_list, list)
+    assert '_hash_not_naive' in second_ht.hashing_algorithm
+
+
+def test_hash_table_init_bucket_size(create_ht):
+    """Test our init sets up a hash_list attribute that is a list."""
+    assert len(create_ht.hash_list) == 10
+
+
+def test_hash_table_init_error_when_algorithm_not_found():
+    """Test our init sets up a hash_list attribute that is a list."""
+    with pytest.raises(KeyError) as e:
+        HashTable(hashing_algorithm='unknown')
+    assert 'Hashing algorithm not found' in str(e)
 
 
 def test_hash_table_hash(create_ht):
     """Test hash table _hash method converts key into a number."""
-    hashed_value = create_ht._hash('a')
-    assert hashed_value == 97
+    hashed_value = create_ht._hash('ab')
+    assert hashed_value == 195
+
+
+def test_hash_table_2nd_hash(second_ht):
+    """Test hash table _hash method converts key into a number."""
+    hashed_value = second_ht._hash_not_naive('ab')
+    assert hashed_value == 9798
 
 
 def test_hash_table_hash_is_int(create_ht):
@@ -48,3 +79,20 @@ def test_hash_table_get_method(create_ht):
 def test_hash_table_get_key_not_in_hash_table(create_ht):
     """Test get method for non existant key returns None."""
     assert create_ht.get('z') is None
+
+
+def test_hash_table_set_method_2nd_hash(second_ht):
+    """Test set method adds key and value to the hash table."""
+    second_ht.set('xy', "Hi Will")
+    assert second_ht.hash_list[1] == [('xy', 'Hi Will')]
+
+
+def test_hash_table_get_method_2nd_hash(second_ht):
+    """Test get method returns value associated with key used."""
+    second_ht.set('xyz', 'Hi Nic')
+    assert second_ht.get('xyz') == 'Hi Nic'
+
+
+def test_hash_table_get_key_not_in_hash_table_2nd_hash(second_ht):
+    """Test get method for non existant key returns None."""
+    assert second_ht.get('z') is None
